16c894a81a7a0f3678bb5b0c9d5f63d6c15b0c61
Pavel Prorovsky <prorovsky@gmail.com>
2017-01-08 13:01:10 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#1 Request Header Parser Microservice
+
+User Story: I can get the IP address, language and operating system for my browser.
+
+you can try it on [heroku](https://prorovsky-timestamp-api.herokuapp.com)
---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+    app = express(),
+    data = {
+        ipAddress: '',
+        language: '',
+        software: ''
+    };
+
+app.get('/api/whoami', function(req, res){
+
+    data.ipAddress = req.ip;
+    data.language = req.headers['accept-language'].split(",")[0];
+    data.software = req.headers['user-agent'].match(/(\(.+?\))/)[0];
+    res.json(data);
+
+});
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Server started...')
+});
