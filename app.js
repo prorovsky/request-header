640ef65bca2c1403674104e61979276da109ef52
Pavel Prorovsky <prorovsky@gmail.com>
2017-01-08 13:43:23 +0300

    replace ip address assginment

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ var express = require('express'),
     };
 
 app.get('/api/whoami', function(req, res){
-
-    // data.ipAddress = req.ip;
-    // data.ipAddress = req.connection.remoteAddress;
-    data.ipAddress = req.headers['x-forwarded-for'].split(',')[0]
+    data.ipAddress = req.headers['x-forwarded-for'].split(',')[0] || req.ip;
     data.language = req.headers['accept-language'].split(",")[0];
     data.software = req.headers['user-agent'].match(/(\(.+?\))/)[0];
     res.json(data);
-
 });
 
 app.listen(process.env.PORT || 3000, function(){
