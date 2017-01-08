07c07680a1bcf7fe04a4fd6c17e2c24e1fbbf2f1
Pavel Prorovsky <prorovsky@gmail.com>
2017-01-08 13:26:40 +0300

    trying new ip address assign

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 User Story: I can get the IP address, language and operating system for my browser.
 
-you can try it on [heroku](https://prorovsky-timestamp-api.herokuapp.com/api/whoami)
+you can try it on [heroku](https://prorovsky-request-header.herokuapp.com/api/whoami)
--- app.js
+++ app.js
@@ -8,7 +8,8 @@ var express = require('express'),
 
 app.get('/api/whoami', function(req, res){
 
-    data.ipAddress = req.ip;
+    // data.ipAddress = req.ip;
+    data.ipAddress = req.connection.remoteAddress;
     data.language = req.headers['accept-language'].split(",")[0];
     data.software = req.headers['user-agent'].match(/(\(.+?\))/)[0];
     res.json(data);
